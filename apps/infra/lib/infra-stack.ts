8e6f8e186933188615208ce9ce3a73de3bb71024
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2025-07-18 21:19:29 +0300

    fix amplify iam stack role

--- apps/infra/lib/infra-stack.ts
+++ apps/infra/lib/infra-stack.ts
@@ -5,6 +5,7 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -20,7 +21,17 @@ export class InfraStack extends Stack {
 
     const amplifyAppName = 'Frontend';
 
+    const amplifyRole = new iam.Role(this, 'AmplifyRole', {
+      assumedBy: new iam.ServicePrincipal('amplify.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'AdministratorAccess-Amplify'
+        ),
+      ],
+    });
+
     const amplifyApp = new App(this, amplifyAppName, {
+      role: amplifyRole,
       environmentVariables: {
         AMPLIFY_MONOREPO_APP_ROOT: 'apps/web',
       },
