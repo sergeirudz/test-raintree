ab19170a35b90aafc0a71f01c487144b24101ead
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2025-07-18 21:13:22 +0300

    fix stack default account name

--- apps/infra/bin/infra.ts
+++ apps/infra/bin/infra.ts
@@ -5,4 +5,8 @@ import { InfraStack } from '../lib/infra-stack';
 const app = new cdk.App();
 new InfraStack(app, 'InfraStack', {
   stackName: 'test-raintree',
+  env: {
+    account: '286668516152',
+    region: 'us-east-1',
+  },
 });
